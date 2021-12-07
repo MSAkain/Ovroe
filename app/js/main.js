690db68b24e36c92ee680642994e3370835580d6
MSAkain <theggodisthere@gmail.com>
2021-12-07 09:24:37 +0300

    added 3 sections and fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,13 @@ $(function () {
 		nextArrow: '<button type="button" class="slick-arrow arrow-left">next</button>',
 	});
 
+	$('.category__slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-arrow arrow-right">prev</button>',
+		nextArrow: '<button type="button" class="slick-arrow arrow-left">next</button>',
+	});
+
 
 	$('.header__input').click(function () {
 		$(this).toggleClass('header__input--active')
