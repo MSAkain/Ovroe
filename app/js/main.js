b025d2160b12ba9233b96d338b2a74098b52fb4e
MSAkain <theggodisthere@gmail.com>
2021-12-09 10:59:26 +0300

    added filters and optimization

--- app/js/main.js
+++ app/js/main.js
@@ -21,9 +21,45 @@ $(function () {
 		$('.header__search').toggleClass('header__search--active')
 	});
 
-	$('.catalog__category--color').click(function () {
+	$('.catalog__category').click(function () {
 		$($(this)).toggleClass('catalog__category--active')
-		$('.filter-color').toggleClass('filter-color--active')
+		$('.filter-color').toggleClass('filter--active')
 	});
 
+	$('.catalog__category').click(function () {
+		$($(this)).toggleClass('catalog__category--active')
+		$('.filter-size').toggleClass('filter--active')
+	});
+
+	$('.catalog__category').click(function () {
+		$($(this)).toggleClass('catalog__category--active')
+		$('.filter-browse').toggleClass('filter--active')
+	});
+
+	$('.catalog__select').click(function () {
+		$('.sort').toggleClass('sort--active')
+		$($(this)).toggleClass('catalog__select--active')
+	});
+
+
+	$('.catalog__category--color').click(function () {
+		$('.filter-size').removeClass('filter--active')
+		$('.filter-browse').removeClass('filter--active')
+		$('.catalog__category--size').removeClass('catalog__category--active')
+		$('.catalog__category--browse').removeClass('catalog__category--active')
+	});
+
+	$('.catalog__category--size').click(function () {
+		$('.filter-color').removeClass('filter--active')
+		$('.filter-browse').removeClass('filter--active')
+		$('.catalog__category--color').removeClass('catalog__category--active')
+		$('.catalog__category--browse').removeClass('catalog__category--active')
+	});
+
+	$('.catalog__category--browse').click(function () {
+		$('.filter-color').removeClass('filter--active')
+		$('.filter-size').removeClass('filter--active')
+		$('.catalog__category--color').removeClass('catalog__category--active')
+		$('.catalog__category--size').removeClass('catalog__category--active')
+	});
 });
